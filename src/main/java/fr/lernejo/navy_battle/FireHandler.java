62ab1bdf04f9de0cbae0dfa0ace0fb9504f7a22b
Jonathan <jonathan.ren@protonmail.com>
2022-03-13 15:06:07 +0100

    Implement Fire API server

--- src/main/java/fr/lernejo/navy_battle/FireHandler.java
+++ src/main/java/fr/lernejo/navy_battle/FireHandler.java
@@ -5,14 +5,39 @@ import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 public class FireHandler implements HttpHandler {
+
+    public Map<String, String> queryToMap(String query) {
+        if(query == null) {
+            return null;
+        }
+        Map<String, String> result = new HashMap<>();
+        for (String param : query.split("&")) {
+            String[] entry = param.split("=");
+            if (entry.length > 1) {
+                result.put(entry[0], entry[1]);
+            }else{
+                result.put(entry[0], "");
+            }
+        }
+        return result;
+    }
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
         if ("GET".contentEquals(exchange.getRequestMethod())) {
-            exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, "OK".length());
-            exchange.getResponseBody().write("OK".getBytes());
+            Map<String, String> params = queryToMap(exchange.getRequestURI().getQuery());
+            if (!Objects.isNull(params) && !Objects.isNull(params.get("cell"))) {
+                exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, "OK".length());
+                exchange.getResponseBody().write("OK".getBytes());
+            } else {
+                exchange.sendResponseHeaders(HttpURLConnection.HTTP_BAD_REQUEST, "Bad request !".length());
+                exchange.getResponseBody().write("Bad request !".getBytes());
+            }
         } else {
             exchange.sendResponseHeaders(HttpURLConnection.HTTP_NOT_FOUND, "Not found !".length());
             exchange.getResponseBody().write("Not found !".getBytes());
