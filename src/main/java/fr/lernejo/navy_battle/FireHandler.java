8fae3702db684c1f915867f920f81a48f3888cb9
Jonathan <jonathan.ren@protonmail.com>
2022-03-27 18:09:58 +0200

    Test sendFire on FireHandler route

--- src/main/java/fr/lernejo/navy_battle/FireHandler.java
+++ src/main/java/fr/lernejo/navy_battle/FireHandler.java
@@ -53,8 +53,8 @@ public class FireHandler implements HttpHandler {
             Map<String, String> params = queryToMap(exchange.getRequestURI().getQuery());
             if (!Objects.isNull(params) && !Objects.isNull(params.get("cell")) && !params.get("cell").equals("")) {
                 sendResponse(exchange, response.toString(), HttpURLConnection.HTTP_OK);
-                // try { if (this.running.get("running") && this.game.getUrl().length() != 0) this.sendFire(this.game.getUrl(), "A2"); }
-                // catch (InterruptedException e) { e.printStackTrace(); }
+                try { if (this.running.get("running") && this.game.getUrl().length() != 0) this.sendFire(this.game.getUrl(), "F1"); }
+                catch (InterruptedException e) { e.printStackTrace(); }
             } else {
                 sendResponse(exchange, "Bad request !", HttpURLConnection.HTTP_BAD_REQUEST);
             }
@@ -70,9 +70,8 @@ public class FireHandler implements HttpHandler {
         exchange.close();
     }
 
-    /*
     public String sendFire(String adversaryURL, String cell) throws IOException, InterruptedException {
-        if (new Random().nextInt(2) == 1) { this.running.put("running", false); }
+        if (this.running.get("running")) { this.running.put("running", false); }
         HttpClient client = this.game.getClient();
         HttpRequest fireRequest = HttpRequest.newBuilder()
             .uri(URI.create(adversaryURL + "/api/game/fire?cell=" + cell))
@@ -82,5 +81,4 @@ public class FireHandler implements HttpHandler {
         HttpResponse<String> response = client.send(fireRequest, HttpResponse.BodyHandlers.ofString());
         return response.body();
     }
-    */
 }
